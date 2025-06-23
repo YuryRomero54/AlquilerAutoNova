27d3d4d8d407fb3d2ee4b72ab5a3faf932ac9cf7
YuryRomero54 <vr545329@gmail.com>
2025-06-23 17:40:49 -0500

    subo backend y frontend con todas las tareas

--- src/main/java/com/example/demo/Constantes/AutoNovaConstantes.java
+++
@@ -1,9 +0,0 @@
-package com.example.demo.Constantes;
-
-public class AutoNovaConstantes {
-
-	
-	public static final String ALGO_SALIO_MAL= "Algo salio mal";
-	
-	public static final String Datos_Incorrectos= "Datos incorrectos";
-}
